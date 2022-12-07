ae34c76ce21dbe73e6012eb592ca2423344eaf66
Mauricio Olivares <m.olivares.r@gmail.com>
2022-12-07 20:46:54 +0100

    Step 3

--- string_calculator.py
+++ string_calculator.py
@@ -1,35 +1,46 @@
 # Step 1: Simple Calculator
 # Create a simple String calculator with a single method:
 
+
+def split_numbers(string_numbers):        
+    custom_separator = ''
+    comma_separator = ','
+    newline_separator = '\n'
+
+    def separate_numbers(string_numbers, separator):
+        separated_numbers = string_numbers.split(newline_separator)              
+        joined_numbers = separator.join(separated_numbers)
+        separated_numbers = joined_numbers.split(separator)
+        return separated_numbers
+        
+    if (len(string_numbers) > 3):
+        if (string_numbers[0] and string_numbers[1]) == '/':
+            custom_separator = string_numbers[2]
+            string_numbers_filtered = string_numbers[3:]
+            separated_numbers = separate_numbers(string_numbers_filtered, custom_separator)
+            return separated_numbers
+        else:
+            separated_numbers = separate_numbers(string_numbers, comma_separator)
+            return separated_numbers
+    else:             
+        separated_numbers = separate_numbers(string_numbers, comma_separator)
+        return separated_numbers
+
 class StringCalculator:
 
     def __init__(self, string_numbers):
         self.numbers = string_numbers
-# class StringCalculator {
-#     int Add(string numbers);
-# }
-# The method can take 1 or 2 comma-separated numbers, and will return their sum.
 
-# The method returns 0 when passed the empty string.
     def Add(string_numbers):
         result = 0
-        separated_numbers = string_numbers.split(",")
-        # print(separated_numbers,"d")
+        separated_numbers = split_numbers(string_numbers)
+        print(separated_numbers,'dd')
         for num in separated_numbers:
             try:
                 num_cache = int(num)
             except:
                 result = 0
-                # print(result,"c")
                 return result
             else:
                 result = result + num_cache
-            
-        # print(result,"b")
         return result
-# Example:
-
-# Add("") // 0
-# Add("4") // 4
-# Add("1,2") // 3
-# Start with the simplest test case of an empty string and move to 1 and two numbers.
--- test_assert_examples.py
+++ test_assert_examples.py
@@ -1,25 +1,14 @@
-# from calculator import sum
 from string_calculator import StringCalculator
-
-# def test_uppercase():
-#     assert "loud noises".upper() == "LOUD NOISES"
-
-# def test_reversed():
-#     assert list (reversed([1,2,3,4])) == [4,3,2,1]
-
-# def test_some_primes():
-#     assert 37 in {
-#         num 
-#         for num in range(2,50)
-#         if not any(num % div ==0 for div in range(2,num))
-#     }
-
-# # def test_sum ():
-# #     assert sum("a",3)
 
 def test_add_empty ():
     assert StringCalculator.Add("") == 0
 def test_add_single_number ():
     assert StringCalculator.Add("4") == 4
-def test_add_two_number ():
+def test_add_two_numbers ():
     assert StringCalculator.Add("1,2") == 3
+def test_add_many_numbers ():
+    assert StringCalculator.Add("1,2,3,4,5,6,7,8,9,10") == 55
+def test_add_newlines_separator ():
+    assert StringCalculator.Add("1\n2,3")
+def test_add_custom_separator():
+    assert StringCalculator.Add("//;1\n2;3") == 6
